152a9177ab4c09680c98bbf6c27e97b43136e8dd
josnoc <josnocpp@gmail.com>
2018-04-10 16:52:01 -0400

    save for cloning to new pc

--- server/server.js
+++ server/server.js
@@ -106,6 +106,18 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users/me',(req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if(!user) {
+            console.log('no user');
+        }
+
+        res.send(user);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
