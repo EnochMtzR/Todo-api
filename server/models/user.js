5f65b396a4e9a5d8a2fab264bc0f0e630afe606f
josnoc <josnocpp@gmail.com>
2018-04-11 17:52:36 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,25 @@ UserSchema.statics.findByToken = function (token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return bcrypt.compare(password, user.password)
+        .then((ret) => {
+            if(!ret){
+                return Promise.reject();
+            }
+
+            return user;
+        }).catch((e) => Promise.reject(e));
+    });
+}
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
